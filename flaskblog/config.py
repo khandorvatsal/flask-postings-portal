d5cd6f0ab74431b06a49c693b168e71db4015b8b
Vatsal Khandor <vatsal.khandor@bombaysoftwares.com>
2022-05-01 20:10:01 +0530

    project done

---
+++ flaskblog/config.py
@@ -0,0 +1,10 @@
+import os
+
+class Config:
+    SECRET_KEY = 'eebd20818c12e189838304dae023c115'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get('EMAIL_USER')
+    MAIL_PASSWORD = os.environ.get('EMAIL_PASS')
